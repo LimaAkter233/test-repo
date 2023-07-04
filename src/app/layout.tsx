1ad1cbff4ea5fb293c54611fbef452b586064ded
lima-cpl <akter-lima@cloudproductionltd.com>
2023-07-04 10:09:19 +0600

    test by ...

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>{children}
+      <h1>hi</h1>
+      
+      </body>
     </html>
   )
 }
